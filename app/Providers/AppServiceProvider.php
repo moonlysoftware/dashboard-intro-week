e8f266905f0f593fca874d9f12aa343680ed4e43
thijs-kuipers <thijs@crewontour.com>
2026-02-12 06:40:35 -0800

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Vite;
-use Illuminate\Support\Facedes\URL;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
