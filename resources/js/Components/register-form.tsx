46f3dd08498fe16e049f3d76237eac64ff895fb9
Sem van der Heijden <sem.heijden@moonlysoftware.com>
2026-02-16 11:27:33 +0100

    remove link

--- resources/js/Components/register-form.tsx
+++ resources/js/Components/register-form.tsx
@@ -116,10 +116,6 @@ export function RegisterForm({
           </div>
         </CardContent>
       </Card>
-      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
-        By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
-        and <a href="#">Privacy Policy</a>.
-      </div>
     </div>
   )
 }
